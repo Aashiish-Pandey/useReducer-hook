9638f767274da31b2e0add4afec5e12d4c0348df
Aashiish-Pandey <ash006pandey@gmail.com>
2021-04-08 18:51:19 +0530

    manage state using useState()

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import './App.css'
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount(prevCount => prevCount + 1);
+  }
+
+  function decrement() {
+    setCount(prevCount => prevCount - 1);
+  }
+
+  return (
+    <>
+      <button onClick={decrement}>-</button>
+      <span>{count}</span>
+      <button onClick={increment}>+</button>
+    </>
+  )
+}
